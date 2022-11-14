2c156fcb25e65e8cf9b7aa7e26daf80211a5b651
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-14 20:05:10 -0300

    style: setting project color variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,5 +12,18 @@ export const GlobalStyle = createGlobalStyle`
 
     html {
         font-family: 'Inter', sans-serif;
+
+        // Colors
+        --color-primary: #1b4b66;
+        --color-primary-tint: #639599;
+        --color-error: #b00020;
+        --color-highlight: #ff8c4b;
+        --color-accent: #f4f4f4;
+        --color-dark: #13101e;
+        --color-bright: #ffffff;
+        --color-grey: #f1f1f1;
+        --color-light-text: #b6b6b6;
+        --color-high-emphasis: #171520;
+        --color-low-emphasis: #626262;
     }
 `;
