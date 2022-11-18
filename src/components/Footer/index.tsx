42e4f0ba045cb01369e752a4e75118ced7431807
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-18 11:59:50 -0300

    feat: created footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,86 @@
+import { Link } from "react-router-dom";
+import { 
+    ColumnsContainer,
+    FooterContainer, 
+    ItemsList, 
+    ItemsListRow, 
+    SocialContainer, 
+    SocialList 
+} from "./styles";
+
+export const Footer: React.FC = () => {
+
+    return (
+        
+        <FooterContainer>
+            <ColumnsContainer>
+                <div>
+                    <h3>Shop by Category</h3>
+                    <nav>
+                        <ItemsList>
+                            <li><Link to="/">Skincare</Link></li>
+                            <li><Link to="/">Personal Care</Link></li>
+                            <li><Link to="/">Handbags</Link></li>
+                            <li><Link to="/">Apparels</Link></li>
+                            <li><Link to="/">Watches</Link></li>
+                            <li><Link to="/">Eye Wear</Link></li>
+                            <li><Link to="/">Jewellery</Link></li>
+                        </ItemsList>
+                    </nav>
+                </div>
+                <div className='about-menu'>
+                    <h3>About</h3>
+                    <nav>
+                        <ItemsListRow>
+                            <li><Link to="/">Contact Us</Link></li>
+                            <li><Link to="/">About Us</Link></li>
+                            <li><Link to="/">Careers</Link></li>
+                            <li><Link to="/">Press</Link></li>
+                        </ItemsListRow>
+                    </nav>
+                </div>
+                <div>
+                    <h3>Policy</h3>
+                    <nav>
+                        <ItemsListRow>
+                            <li><Link to="/">Return</Link></li>
+                            <li><Link to="/">Terms of Use</Link></li>
+                            <li><Link to="/">Sitemap</Link></li>
+                            <li><Link to="/">Security</Link></li>
+                            <li><Link to="/">Privacy</Link></li>
+                            <li><Link to="/">EPR Compliance</Link></li>
+                        </ItemsListRow>
+                    </nav>
+                </div>
+            </ColumnsContainer>
+            <SocialContainer>
+                <nav>
+                    <SocialList>
+                        <li>
+                            <a href='https://www.facebook.com/' target='_blank'>
+                                <img src='/assets/img/icons/facebook-icon.svg' alt='facebook icon' />
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://www.instagram.com/' target='_blank'>
+                                <img src='/assets/img/icons/instagram-icon.svg' alt='instagram icon' />
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://twitter.com/' target='_blank'>
+                                <img src='/assets/img/icons/twitter-icon.svg' alt='twitter icon' />
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://www.youtube.com/' target='_blank'>
+                                <img src='/assets/img/icons/youtube-icon.svg' alt='youtube icon' />
+                            </a>
+                        </li>
+                    </SocialList>
+                </nav>
+                <p className='footer-country'>United States</p>
+                <p className='footer-copyright'>© 2022 | Cora Leviene All Rights Reserved</p>
+            </SocialContainer>
+        </FooterContainer>
+    );  
+};
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import { font } from "../../styles/mixins";
+
+export const FooterContainer = styled.footer`
+
+    display: flex;
+    justify-content: space-between;
+    background-color: var(--color-primary);
+    padding: 60px 32px; 
+
+    @media (max-width: 985px) {
+        flex-direction: column;
+        padding: 0; 
+    }
+`;
+
+export const ColumnsContainer = styled.div`
+
+    display: flex;
+    gap: 68px;
+
+    h3 {
+        font-size: 1rem;
+        color: var(--color-bright);
+        margin-bottom: 12px;
+    }
+
+    a {
+        color: var(--color-light-text);
+
+        &:hover {
+            color: var(--color-bright);
+        }
+    }
+
+    @media (max-width: 985px) {
+        flex-direction: column;
+        gap: 24px;
+        padding: 24px 16px; 
+        margin-bottom: 12px;
+
+        .about-menu {
+            order: 3;
+        }
+    }
+`;
+
+export const ItemsList = styled.ul`
+
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+`;
+
+export const ItemsListRow = styled.ul`
+
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    @media (max-width: 985px) {
+        flex-direction: row;
+        flex-wrap: wrap;
+
+        li {
+
+            &:not(:first-child) {
+
+                &::before {
+                    content: '| ';
+                    color: var(--color-light-text);
+                }
+            }
+        }
+    }
+`
+
+export const SocialContainer = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+
+    .footer-country {
+        display: flex;
+        align-items: center;
+        gap: 8px;
+        ${font.titleMedium};
+        color: var(--color-bright);
+        margin-bottom: 8px;
+
+        &::before {
+            content: '';
+            display: block;
+            background-image: url('/assets/img/icons/location-icon.svg');
+            width: 24px;
+            height: 24px;
+        }
+    }
+
+    .footer-copyright {
+        ${font.titleMedium};
+        color: var(--color-light-text);
+    }
+
+    @media (max-width: 985px) {
+        align-items: flex-start;
+        padding: 24px 16px;
+        border-top: 1px solid var(--color-primary-tint);
+    }
+`
+
+export const SocialList = styled.ul`
+
+    display: flex;
+    gap: 1rem;
+    margin-bottom: 26px;
+`
