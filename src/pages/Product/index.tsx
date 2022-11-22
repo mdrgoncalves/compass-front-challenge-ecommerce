9191bca14dae2319e5b7164588e7168302b546ec
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-22 18:22:38 -0300

    feat: created product page

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,90 @@
+import { useParams } from "react-router-dom";
+import productsData from "../../data/productsData";
+
+import { Button} from "../../components/Button";
+import { ProductSlider } from "../../components/Desktop/ProductSlider";
+import { PathPage } from "../../components/PathPage";
+import { ProductDesc } from "../../components/ProductDesc";
+import { ProductContainer } from "./styles";
+import { ProductTabs } from "../../components/Desktop/ProductTabs";
+import { ProductCarousel } from "../../components/Mobile/ProductCarousel";
+import { BackButton } from "../../components/Mobile/BackButton";
+import { Spacer } from "../../components/Mobile/Spacer";
+import { CollapseButton } from "../../components/Mobile/CollapseButton";
+import { DescriptionTab } from "../../components/Desktop/ProductTabs/DescriptionTab";
+import { InviteBlock } from "../../components/Mobile/InviteBlock";
+import { MobileButtonTab } from "../../components/Mobile/MobileButtonTab";
+import { TrendingCarousel } from "../../components/Mobile/TrendingCarousel";
+import { ProductMobileButtons } from "../../components/Mobile/ProductMobileButtons";
+import { useWidth } from "../../utils/useWidth";
+
+
 export const Product: React.FC = () => {
+
+    const { productId } = useParams<{ productId: string }>();
+
+    const thisProduct = productsData.find(product => product.id === Number(productId));
+    const { img, name, description, price, category } = thisProduct!;
+    const images = [img, img, img];
+
     return (
-        <h1>Product</h1>
+
+        <ProductContainer>
+            <div className='page-path'>
+                <BackButton />
+                <PathPage
+                    paths={[`${category}`, `${name}`]}
+                />
+            </div>
+            <div className='slider'>
+                <ProductCarousel
+                    images={images}
+                />
+                <ProductSlider />
+            </div>
+            <div className='desc'>
+                <ProductDesc 
+                    name={name}
+                    description={description}
+                    price={price}
+                />
+                <div className='desc__buttons'>
+                    <Button color='primary' iconType='bag'>
+                        Add to bag
+                    </Button>
+                    <Button color='secondary' iconType='wishlist'>
+                        Add To Wishlist
+                    </Button>
+                </div>
+            </div>
+            <div className='tabs'>
+                <ProductTabs /> 
+            </div>
+
+            {useWidth({
+                renderMobile: () => (
+                    <>
+                    <Spacer />
+                    <CollapseButton label='Product Description'>
+                        <DescriptionTab />
+                    </CollapseButton>
+                    <Spacer />
+                    <MobileButtonTab 
+                        label='Ratings & Reviews'
+                        route='/404'
+                    />
+                    <Spacer />
+                    <InviteBlock />
+                    <Spacer />
+                    <TrendingCarousel />
+                    <ProductMobileButtons />
+                    </>
+                ),
+                renderDesktop: () => (
+                    null
+                )
+            })}
+
+        </ProductContainer>
     )
 };
---
+++ src/pages/Product/styles.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+export const ProductContainer = styled.div`
+
+    display: grid; 
+    grid-template-columns: 1fr minmax(300px, 1fr); 
+    grid-template-rows: auto auto auto; 
+    gap: 1rem 0px; 
+    grid-template-areas:
+        "page-path page-path" 
+        "slider desc"
+        "tabs tabs"; 
+    padding: 0 20px;
+
+    .page-path {
+        grid-area: page-path;
+    }
+
+    .slider {
+        grid-area: slider;
+    }
+
+    .desc {
+        grid-area: desc;
+
+        &__buttons {
+            display: flex;
+            gap: 24px;
+        }
+    }
+
+    .tabs {
+        grid-area: tabs;
+        margin-top: 55px;
+        margin-bottom: 95px;
+    }
+
+    @media (max-width: 985px) {
+        grid-template-columns: 1fr;
+        grid-template-rows: auto auto auto;
+        gap: 0.5rem 0px;
+        grid-template-areas:
+            "page-path"
+            "slider"
+            "desc";
+        padding: 0 1rem;
+        margin-bottom: 55px;
+
+        .page-path {
+            padding: 1rem 1rem 1rem 0;
+        }
+
+        .slider {
+            margin-bottom: -19px;
+        }
+
+        .desc {
+            grid-area: desc;
+            width: calc(100vw - 32px);
+
+            &__buttons {
+                display: none;
+            }
+        }
+
+        .tabs {
+            display: none;
+        }
+    }
+`;
