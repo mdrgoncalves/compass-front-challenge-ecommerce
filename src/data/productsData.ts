4eedc5237697fbc3627f358f9d7bb56fe769ca00
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-23 20:58:56 -0300

    chore: changes in product data

--- src/data/productsData.ts
+++ src/data/productsData.ts
@@ -34,33 +34,33 @@ export default [
     {
         id: 5,
         img: '/assets/img/products/grande-bag.png',
-        name: 'Collosal',
-        description: 'Tall Heels',
-        price: '$40.00',
-        category: 'watches'
+        name: 'Grande',
+        description: 'Blossom Pouch',
+        price: '$39.49',
+        category: 'handbags'
     },
     {
         id: 6,
-        img: '/assets/img/products/grande-bag.png',
-        name: 'Boker',
-        description: 'Brown Bag',
-        price: '$34.55',
+        img: '/assets/img/products/coach-bag.png',
+        name: 'Coach',
+        description: 'Leather Coach Bag',
+        price: '$54.69',
         category: 'handbags'
     },
     {
         id: 7,
-        img: '/assets/img/products/grande-bag.png',
-        name: 'Boker',
-        description: 'Brown Bag',
-        price: '$34.55',
+        img: '/assets/img/products/remus-bag.png',
+        name: 'Remus',
+        description: 'Brown Strap Bag',
+        price: '$57.00',
         category: 'handbags'
     },
     {
         id: 8,
-        img: '/assets/img/products/grande-bag.png',
-        name: 'Boker',
-        description: 'Brown Bag',
-        price: '$34.55',
+        img: '/assets/img/products/boujee-bag.png',
+        name: 'Boujee',
+        description: 'Black Bag',
+        price: '$54.69',
         category: 'handbags'
     },
     {
@@ -86,5 +86,77 @@ export default [
         description: 'Brown Strap Bag',
         price: '$57.00',
         category: 'handbags'
+    },
+    {
+        id: 12,
+        img: '/assets/img/products/boujee-bag.png',
+        name: 'Boujee',
+        description: 'Black Bag',
+        price: '$54.69',
+        category: 'handbags'
+    },
+    {
+        id: 13,
+        img: '/assets/img/products/grande-bag.png',
+        name: 'Grande',
+        description: 'Blossom Pouch',
+        price: '$39.49',
+        category: 'handbags'
+    },
+    {
+        id: 14,
+        img: '/assets/img/products/coach-bag.png',
+        name: 'Coach',
+        description: 'Leather Coach Bag',
+        price: '$54.69',
+        category: 'handbags'
+    },
+    {
+        id: 15,
+        img: '/assets/img/products/remus-bag.png',
+        name: 'Remus',
+        description: 'Brown Strap Bag',
+        price: '$57.00',
+        category: 'handbags'
+    },
+    {
+        id: 16,
+        img: '/assets/img/products/boujee-bag.png',
+        name: 'Boujee',
+        description: 'Black Bag',
+        price: '$54.69',
+        category: 'handbags'
+    },
+    {
+        id: 17,
+        img: '/assets/img/products/grande-bag.png',
+        name: 'Grande',
+        description: 'Blossom Pouch',
+        price: '$39.49',
+        category: 'watches'
+    },
+    {
+        id: 18,
+        img: '/assets/img/products/coach-bag.png',
+        name: 'Coach',
+        description: 'Leather Coach Bag',
+        price: '$54.69',
+        category: 'skincare'
+    },
+    {
+        id: 19,
+        img: '/assets/img/products/remus-bag.png',
+        name: 'Remus',
+        description: 'Brown Strap Bag',
+        price: '$57.00',
+        category: 'jewellery'
+    },
+    {
+        id: 20,
+        img: '/assets/img/products/boujee-bag.png',
+        name: 'Boujee',
+        description: 'Black Bag',
+        price: '$54.69',
+        category: 'apparels'
     },
 ]
