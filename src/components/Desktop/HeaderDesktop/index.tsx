5e93943abc7dddb8c5b1f6c64a171be2190c158b
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-23 21:08:32 -0300

    feat: added notification symbol when there is item in the bag

--- src/components/Desktop/HeaderDesktop/index.tsx
+++ src/components/Desktop/HeaderDesktop/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { NavLink } from "react-router-dom"
+import { BagState } from "../../../context/BagContext"
 
 import { IconButton } from "../../IconButton"
 import { BasketModal } from "../BasketModal"
@@ -9,6 +10,8 @@ import { ButtonsContainer, HeaderContainer, ListContainer, MenuContainer } from 
 export const HeaderDesktop: React.FC = () => {
 
     const [isBasketOpen, setIsBasketOpen] = useState(false)
+
+    const { state: { bag } } = BagState();
 
     useEffect(() => {
         if (isBasketOpen) {
@@ -52,7 +55,7 @@ export const HeaderDesktop: React.FC = () => {
                 <IconButton iconSrc='/assets/img/wishlist-icon.svg' alt='favorite icon'/>
                 <IconButton iconSrc='/assets/img/profile-icon.svg' alt='profile icon'/>
                 <IconButton 
-                    iconSrc='/assets/img/bag-icon.svg' 
+                    iconSrc={bag.length > 0 ? '/assets/img/icons/bag-with-notification-icon.svg' : '/assets/img/bag-icon.svg' }
                     alt='bag icon'
                     onClick={() => setIsBasketOpen(true)}
                 />
