d662f4c663ae35ec52bae137108c29519b7c185d
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-28 17:33:25 -0300

    api: created product control

---
+++ api/controllers/ProductController.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from "express";
+import product from '../models/Product.js';
+
+class ProductController {
+
+    static async createProduct(req: Request, res: Response) {
+
+        const bodyData = req.body;
+
+        try {
+            const newProduct = await product.create(bodyData);
+            return res.status(201).json(newProduct);
+        } catch (error) {
+            return res.status(400).
+                json({ message: 'Product creation was unsuccessful' });
+        }
+    }
+
+    static async updateProduct(req: Request, res: Response) {
+
+        const { product_id } = req.params;
+        const bodyData = req.body;
+
+        try {
+            const updatedProduct = await product.findByIdAndUpdate
+                (product_id, bodyData, { new: true });
+            return res.status(200).json(updatedProduct);
+        } catch (error) {
+            return res.status(400).
+                json({ message: 'Product update was unsuccessful' });
+        }
+    }
+
+    static async deleteProduct(req: Request, res: Response) {
+
+        const { product_id } = req.params;
+
+        try {
+            await product.findByIdAndDelete(product_id);
+            return res.status(200).json({ message: 'Product deleted' });
+        } catch (error) {
+            return res.status(404).
+                json({ message: 'Product not found' });
+        }
+    }
+
+    static async getProductById(req: Request, res: Response) {
+
+        const { product_id } = req.params;
+
+        try {
+            const foundProduct = await product.findById
+                (product_id);
+            return res.status(200).json(foundProduct);
+        } catch (error) {
+            return res.status(404).
+                json({ message: 'Product not found' });
+        }
+    }
+
+    static async getProducts(req: Request, res: Response) {
+
+        try {
+            const foundProducts = await product.find();
+            return res.status(200).json(foundProducts);
+        } catch (error) {
+            return res.status(404).
+                json({ message: 'Products not found' });
+        }
+    }
+
+    static async getProductsByCategory(req: Request, res: Response) {
+
+        const { category } = req.params;
+
+        try {
+            const foundProducts = await product.find
+                ({ productCategory: category });
+            return res.status(200).json(foundProducts);
+        } catch (error) {
+            return res.status(404).
+                json({ message: 'Products not found' });
+        }
+    }
+}
+
+export default ProductController;
