39ab626137f28a250193510c5b770362c0ea1d54
mdrgoncalves <mdr.rgon@gmail.com>
2022-11-30 19:55:29 -0300

    feat: added search products

--- src/components/Desktop/HeaderDesktop/SearchInput/index.tsx
+++ src/components/Desktop/HeaderDesktop/SearchInput/index.tsx
@@ -1,12 +1,55 @@
-import { InputSearch } from "./styles"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { ProductState } from "../../../../context/ProductContext";
+import { IProduct } from "../../../../types/Products";
+import { InputSearch, ResultsList } from "./styles"
 
 export const SearchInput: React.FC = () => {
+    
+    const { getProducts, products } = ProductState();
+    const [result, setResult] = useState('');
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    useEffect(() => {
+        const search = products.filter((product: IProduct) => 
+            product.productName
+                .toLowerCase()
+                .includes(result.toLowerCase()));
+        setResults(search);
+    }, [result]);
+
+    useEffect(() => {
+        document.addEventListener('click', () => {
+            setResult('');
+        });
+    }, []);
 
     return (
         
+        <>
         <InputSearch 
             type='search' 
             placeholder='Search for products or brands.....' 
+            value={result}
+            onChange={(e) => setResult(e.target.value)}
         />
+        {result && (
+            <ResultsList>
+                {results.map((product: IProduct) => (
+                    <li key={product._id}>
+                        <Link 
+                            to={`/category/${product.productCategory}/${product._id}`}
+                        >
+                            {product.productName}
+                        </Link>
+                    </li>
+                ))}
+            </ResultsList>
+        )}
+        </>
     )
 }
--- src/components/Desktop/HeaderDesktop/SearchInput/styles.ts
+++ src/components/Desktop/HeaderDesktop/SearchInput/styles.ts
@@ -22,3 +22,34 @@ export const InputSearch = styled.input`
         width: 100%;
     }
 `;
+
+export const ResultsList = styled.ul`
+
+    width: 362px;
+    height: auto;
+    overflow-y: scroll;
+    position: absolute;
+    top: 73px;
+    background: var(--color-grey);
+    border-bottom-right-radius: 4px;
+    border-bottom-left-radius: 4px;
+    padding: 0 40px 0.5rem 40px;
+    border: 1px solid var(--color-high-emphasis);
+
+    @media (max-width: 1100px) {
+        width: 100%;
+    }
+
+    li {
+        padding: 0.5rem 0px;
+
+        a {
+            color: var(--color-primary);
+            font-size: 0.875rem;
+        }
+
+        &:hover {
+            background: rgba(0, 0, 0, 0.08);
+        }
+    }
+`;
