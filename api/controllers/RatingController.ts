2f6ea55b8e39901239a2a52be76b78fab0804fcc
mdrgoncalves <mdr.rgon@gmail.com>
2022-12-02 12:49:27 -0300

    api: created logic for rating

---
+++ api/controllers/RatingController.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from "express";
+import rating from "../models/Rating.js";
+
+class RatingController {
+
+    static async createRating(req: Request, res: Response) {
+            
+        const bodyData = req.body;
+
+        try {
+            const newRating = await rating.create(bodyData);
+            return res.status(201).json(newRating);
+        } catch (error) {
+            return res.status(400).
+                json({ message: 'Rating creation was unsuccessful' });
+        }
+    }
+
+    static async getRatings(req: Request, res: Response) {
+
+        try {
+            const allRatings = await rating.find();
+            return res.status(200).json(allRatings);
+        } catch (error) {
+            return res.status(400).
+                json({ message: 'Rating list was unsuccessful' });
+        }
+    }
+
+    static async getRatingById(req: Request, res: Response) {
+
+        const id = req.params.rating_id;
+
+        try {
+            const ratingById = await rating.findById(id);
+            return res.status(200).json(ratingById);
+        } catch (error) {
+            return res.status(400).
+                json({ message: 'Rating list was unsuccessful' });
+        }
+    }
+
+    static async getRatingsByProduct(req: Request, res: Response) {
+
+        const id = req.params.product_id;
+
+        try {
+            const ratingByProduct = await rating.find({ product: id });
+            return res.status(200).json(ratingByProduct);
+        } catch (error) {
+            return res.status(400).
+                json({ message: 'Rating list was unsuccessful' });
+        }
+    }
+    
+    static async getRatingsByUser(req: Request, res: Response) {
+
+        const id = req.params.user_id;
+
+        try {
+            const ratingByUser = await rating.find({ username: id });
+            return res.status(200).json(ratingByUser);
+        } catch (error) {
+            return res.status(400).
+                json({ message: 'Rating list was unsuccessful' });
+        }
+    }
+}
+
+export default RatingController;
