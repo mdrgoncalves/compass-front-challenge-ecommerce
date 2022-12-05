88e434ef69d3968c6f8449a206c82fd0013d129b
mdrgoncalves <mdr.rgon@gmail.com>
2022-12-04 23:17:12 -0300

    feat: created access to wishlist through user context

--- src/context/UserContex.tsx
+++ src/context/UserContex.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { IProduct } from "../types/Products";
 import { api } from "../utils/api";
 
 export const UserContext = createContext({} as any);
@@ -7,6 +8,7 @@ const UserProvider = ({ children }: any) => {
 
     const [userId] = useState('638c11805dab33fd788dde32');
     const [user, setUser] = useState({});
+    const [wishlist, setWishlist] = useState({});
 
     // Get User by ID
     const getUser = async () => {
@@ -19,11 +21,61 @@ const UserProvider = ({ children }: any) => {
         }
     }
 
+    // Get Wishlist by User
+    const getWishlist = async () => {
+
+        try {
+            const { data } = await api.get(`/wishlist/${userId}`);
+            setWishlist(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // Create or Update Wishlist
+    const createOrUpdateWishlist = async (productId: string) => {
+
+        if (Object.keys(wishlist).length === 0) {
+            const body = {
+                "products": [productId]
+            }
+
+            try {
+                const { data } = await api.post(`/wishlist/${userId}`, body);
+                setWishlist(data);
+            } catch (error) {
+                console.error(error);
+            }
+        } else {
+            try {
+                const { data } = await api.put(`/wishlist/${userId}/${productId}`);
+                setWishlist(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
+    // Remove product of Wishlist
+    const removeProductOfWishlist = async (productId: string) => {
+
+        try {
+            const { data } = await api.delete(`/wishlist/${userId}/${productId}`);
+            setWishlist(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <UserContext.Provider value={{ 
             userId,
             getUser,
-            user
+            user,
+            getWishlist,
+            wishlist,
+            createOrUpdateWishlist,
+            removeProductOfWishlist
         }}>
             {children}
         </UserContext.Provider>
