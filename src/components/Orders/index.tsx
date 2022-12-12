e0a1fa7b652ffa7d00bdf46a4d9eac485e1a86fe
mdrgoncalves <mdr.rgon@gmail.com>
2022-12-12 12:28:04 -0300

    feat: created orders section for desktop

---
+++ src/components/Orders/index.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { UserState } from "../../context/UserContex";
+import { useWidth } from "../../utils/useWidth";
+import { ProductTabs } from "../Desktop/ProductTabs";
+import { OrderList } from "./OrderList";
+
+export const Orders: React.FC = () => {
+
+    const { listOrdersByStatus, orders } = UserState();
+    const [currentActiveTab, setCurrentActiveTab] = useState('');
+
+    useEffect(() => {
+        listOrdersByStatus('paid');
+    }, []);
+
+    useEffect(() => {
+        if (currentActiveTab === 'tab1') {
+            listOrdersByStatus('paid');
+        } else if (currentActiveTab === 'tab2') {
+            listOrdersByStatus('processing');
+        } else if (currentActiveTab === 'tab3') {
+            listOrdersByStatus('cancelled');
+        }
+    }, [currentActiveTab, orders]);
+
+    return (
+        
+        <>
+        {useWidth({
+            renderMobile: () => (
+                <>
+                </>
+            ),
+            renderDesktop: () => (
+                <>
+                <ProductTabs 
+                    firstTabName='Completed'
+                    secondTabName='Processing'
+                    thirdTabName='Cancelled'
+                    firstTabChildren={<OrderList orders={orders} />}
+                    secondTabChildren={<OrderList orders={orders} />}
+                    thirdTabChildren={<OrderList orders={orders} />}
+                    setCurrentActiveTab = {setCurrentActiveTab} 
+                />
+                </>
+            ),
+        })}
+        </>
+    );  
+};
